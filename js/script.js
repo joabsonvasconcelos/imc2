f53578222824c8fbc1cafd95adfece0af360f6fd
Joabson Vasconcelos <119760277+joabsonvasconcelos@users.noreply.github.com>
2023-12-24 16:13:03 -0300

    utils js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 import { Modal } from './modal.js'
+import { AlertError } from './alert-error.js'
+import {  IMC, notANumber } from './utils.js'
 
-// variáveis - variables
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
@@ -12,6 +13,15 @@ form.onsubmit = event => {
     const weight = inputWeight.value
     const height = inputHeight.value
 
+    const showAlertError = notANumber(weight)  || notANumber(height)
+
+    if (showAlertError) {
+        AlertError.open()
+        return;
+    }
+
+    AlertError.close()
+
     const result = IMC(weight, height)
     const message = `Seu IMC é de ${result}`
 
@@ -19,6 +29,3 @@ form.onsubmit = event => {
     Modal.open()
 }
 
-function IMC(weight, height) {
-    return (weight / ((height / 100) ** 2)).toFixed(2)
-}
